273929bf0506c0ecb17f929c3b1fc0ea84bdbd4a
Thanh <kimthanh.cao@gmail.com>
2021-09-01 15:32:01 +0200

    setting up access to google spreadsheet

---
+++ run.py
@@ -0,0 +1,17 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDENTIALS = Credentials.from_service_account_file('credentials.json')
+SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
+SHEET = GSPREAD_CLIENT.open('love-sandwiches')
+
+sales = SHEET.worksheet('sales')
+data = sales.get_all_values()
+print(data)
