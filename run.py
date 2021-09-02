8505a6e9338b71e1f193d9a0e0b28bea37a15e21
Thanh <kimthanh.cao@gmail.com>
2021-09-02 12:46:51 +0200

    add function to calculate stock data and update worksheet

--- run.py
+++ run.py
@@ -93,15 +93,49 @@ def update_data_to_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully.\n')
 
+def get_last_5_entries_sales():
+    '''
+    Collects columns of data from sales worksheet, collecting the last 5 entries for each sandwich and returns the data as a list of lists
+    '''
+    sales = SHEET.worksheet('sales')
+    columns = []
+
+    for index in range(1, 7): 
+        # the column method in gspread starts from index 1, not index 0 as normally in list or dict. Therefore we need to give it range from 1 to 7 for the 6 columns
+        column = sales.col_values(index)
+        columns.append(column[-5:]) # index -5 to get the last 5 entries
+    
+    return columns
+
+def calculate_stock_data(data):
+    '''
+    Calculate the average stock for each item type, adding 10%
+    '''
+    new_stock_data = []
+    
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)  # while we know the objective is to find average of the last 5 market sales and can divide specifically by 5, use len(int_column) in case later on we want to change the average of 7 days or something else in the future
+        stock_num = average * 1.1  # adding 10%
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main_function():
     '''
     Run all program functions
     '''
     data = get_sales_data()
     sales_data = [int(num) for num in data]
+    
     update_data_to_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
+
     update_data_to_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_5_entries_sales()
+
+    stock_data = calculate_stock_data(sales_columns)
+    update_data_to_worksheet(stock_data, 'stock')
 
 print('Welcome to Love Sandwiches Data Automation')
 main_function()
