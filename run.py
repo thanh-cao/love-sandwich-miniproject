3af12bae68b198aadde884d716386859456b12f1
Thanh <kimthanh.cao@gmail.com>
2021-09-02 09:24:25 +0200

    add functions to get sales data and validate it

--- run.py
+++ run.py
@@ -12,6 +12,26 @@ SCOPED_CREDENTIALS = CREDENTIALS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDENTIALS)
 SHEET = GSPREAD_CLIENT.open('love-sandwiches')
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    '''Get sales figure input from the user'''
+    print('Please enter sales data from the last market day')
+    print('Data should be six numbers seperated by comma')
+    print('Example: 40, 20, 55, 65, 11, 45')
+
+    data_str = input('Please enter sales data from the last market day: ')
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+def validate_data(values):
+    '''Inside the try, converts all string values into integers. Raises ValurError if strings cannot be converted into int, or if there aren't exactly 6 values'''
+    try:
+        [int(value) for value in values]
+        if len(values) !=6:
+            raise ValueError(
+                f'Exactly 6 values required, you provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again. \n')
+
+
+get_sales_data()
